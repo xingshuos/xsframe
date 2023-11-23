51d7b8372b70388d5d7737aa70de48a2e0dc8d2e
786824455@qq.com <qq786824455>
2023-11-23 15:11:26 +0800

    更新应用切换部分代码

--- app/admin/controller/System.php
+++ app/admin/controller/System.php
@@ -15,6 +15,7 @@ namespace app\admin\controller;
 use xsframe\base\AdminBaseController;
 use xsframe\enum\SysSettingsKeyEnum;
 use xsframe\util\FileUtil;
+use xsframe\util\StringUtil;
 use xsframe\wrapper\AttachmentWrapper;
 use think\facade\Db;
 
@@ -38,18 +39,17 @@ class System extends AdminBaseController
         ];
 
         $field = "am.id,am.module,am.settings," . "m.name,m.identifie,m.logo";
-        $list  = Db::name('sys_account_modules')->alias('am')->field($field)->leftJoin("sys_modules m", "m.identifie = am.module")->where($condition)->order("am.displayorder asc")->page($this->pIndex, $this->pSize)->select()->toArray();
+        $list = Db::name('sys_account_modules')->alias('am')->field($field)->leftJoin("sys_modules m", "m.identifie = am.module")->where($condition)->order("am.displayorder asc")->page($this->pIndex, $this->pSize)->select()->toArray();
         $total = Db::name('sys_account_modules')->alias('am')->field($field)->leftJoin("sys_modules m", "m.identifie = am.module")->where($condition)->count();
         $pager = pagination2($total, $this->pIndex, $this->pSize);
 
         foreach ($list as &$item) {
             $settings = unserialize($item['settings']);
             if (!empty($settings)) {
-
                 if (!empty($settings['basic'])) {
                     if (!empty($settings['basic']['logo'])) {
-                        $basicLogo                 = tomedia($settings['basic']['logo']);
-                        $item['logo']              = $basicLogo;
+                        $basicLogo = tomedia($settings['basic']['logo']);
+                        $item['logo'] = $basicLogo;
                         $settings['basic']['logo'] = $basicLogo;
                     }
                     if (!empty($settings['basic']['name'])) {
@@ -59,6 +59,9 @@ class System extends AdminBaseController
                 $item['settings'] = $settings;
             }
             $item['logo'] = tomedia($item['logo']);
+
+            // 获取后台访问地址
+            $item['url'] = $this->getRealModuleUrl($item['module']);
         }
         $list = set_medias($list, ['logo']);
 
@@ -70,10 +73,35 @@ class System extends AdminBaseController
         return $this->template('index', $result);
     }
 
+    // 获取后端访问入口
+    private function getRealModuleUrl($moduleName)
+    {
+        $realModuleName = realModuleName($moduleName);
+
+        $moduleMenuConfigFile = IA_ROOT . "/app/" . $moduleName . "/config/menu.php";
+        if (is_file($moduleMenuConfigFile)) {
+            $menuConfig = include($moduleMenuConfigFile);
+            $oneMenus = array_slice($menuConfig, 0, 1);
+            $oneMenusKeys = array_keys($oneMenus);
+            $actionUrl = $oneMenus[$oneMenusKeys[0]]['items'][0]['route'];
+
+            if (StringUtil::strexists($actionUrl, "/")) {
+                $actionUrl = "." . $actionUrl;
+            } else {
+                $actionUrl = "/" . $actionUrl;
+            }
+            $url = webUrl('/' . $realModuleName . "/{$oneMenusKeys[0]}{$actionUrl}", ['i' => $this->uniacid]);
+        } else {
+            $url = webUrl('/' . $realModuleName . "/web.index", ['i' => $this->uniacid]);
+        }
+
+        return $url;
+    }
+
     // 基本信息
     public function account()
     {
-        $uniacid         = $this->uniacid;
+        $uniacid = $this->uniacid;
         $accountSettings = $this->settingsController->getAccountSettings($this->uniacid, 'settings');
 
         if ($this->request->isPost()) {
@@ -84,7 +112,7 @@ class System extends AdminBaseController
                 show_json(0, "更新失败");
             }
 
-            $data             = array(
+            $data = array(
                 "name"         => trim($this->params["name"]),
                 "logo"         => trim($this->params["logo"]),
                 "keywords"     => trim($this->params["keywords"]),
@@ -107,11 +135,11 @@ class System extends AdminBaseController
             $this->success(array("url" => webUrl("account", ['tab' => str_replace("#tab_", "", $this->params['tab'])])));
         }
 
-        $attachmentPath  = IA_ROOT . "/public/attachment/images/{$this->uniacid}";
+        $attachmentPath = IA_ROOT . "/public/attachment/images/{$this->uniacid}";
         $localAttachment = FileUtil::fileDirExistImage($attachmentPath);
 
         $hostList = Db::name('sys_account_host')->where(['uniacid' => $uniacid])->order('id asc')->select();
-        $item     = Db::name('sys_account')->where(['uniacid' => $uniacid])->find();
+        $item = Db::name('sys_account')->where(['uniacid' => $uniacid])->find();
 
         $result = [
             'item'             => $item,
