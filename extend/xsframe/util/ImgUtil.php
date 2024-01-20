698fa5ab7763e57c5fd1f25b63072eced811d14c
zhaoxin <zhaoxin@luyin>
2024-01-21 01:05:01 +0800

    增加ImageMagick库处理图像合成

--- extend/xsframe/util/ImgUtil.php
+++ extend/xsframe/util/ImgUtil.php
@@ -18,33 +18,135 @@ class ImgUtil
 {
     public static function createPosterImg($imgPath, $bgUrl, $data = null)
     {
-        set_time_limit(50);
-        @ini_set('memory_limit', '256M');
+        //  PHP GD 库
+        if (!extension_loaded('imagick')) {
+            set_time_limit(50);
+            @ini_set('memory_limit', '512M');
 
-        $bg = self::createImage($bgUrl);
-        $img_info = getimagesize($bgUrl);
+            $bg       = self::createImage($bgUrl);
+            $img_info = getimagesize($bgUrl);
 
-        $width = $img_info[0];
-        $height = $img_info[1];
+            $width  = $img_info[0];
+            $height = $img_info[1];
 
-        $target = imagecreatetruecolor($width, $height);
-        imagecopy($target, $bg, 0, 0, 0, 0, $width, $height);
-        imagedestroy($bg);
+            $target = imagecreatetruecolor($width, $height);
+            imagecopy($target, $bg, 0, 0, 0, 0, $width, $height);
+            imagedestroy($bg);
 
-        if (!empty($data)) {
-            foreach ($data as $d) {
-                $d = self::getRealData($d);
-                if ($d['type'] == 'img' && !empty($d['src'])) {
-                    $target = self::mergeImage($target, $d['src'], $d['left'], $d['top'], $d['width'], $d['height'], $d['rotate'] ?? 0);
-                } else if ($d['type'] == 'text') {
-                    $target = self::mergeText($target, $d['text'], $d['size'], $d['color'], $d['left'], $d['top']);
+            if (!empty($data)) {
+                foreach ($data as $d) {
+                    $d = self::getRealData($d);
+                    if ($d['type'] == 'img' && !empty($d['src'])) {
+                        $target = self::mergeImage($target, $d['src'], $d['left'], $d['top'], $d['width'], $d['height'], $d['rotate'] ?? 0);
+                    } else if ($d['type'] == 'text') {
+                        $target = self::mergeText($target, $d['text'], $d['size'], $d['color'], $d['left'], $d['top']);
+                    }
                 }
+            }
+
+            imagejpeg($target, $imgPath); // 可接受的有损压缩
+            // imagepng($target, $imgPath); // 原图像无损清晰度
+            imagedestroy($target);
+        } else {
+
+            // 使用compositeImage方法将图片定位到背景上 // 这里使用的是Imagick::COMPOSITE_OVER复合方式，你可以根据需要选择其他方式
+            // 如果你的源图像没有透明度，并且你只是想简单地替换背景图像上的某个区域，那么 Imagick::COMPOSITE_SRC 可能会更快，因为它不考虑目标图像的颜色，只使用源图像的颜色。
+            // 如果你的源图像有透明度，并且你希望它在合成时与目标图像的颜色混合，那么 Imagick::COMPOSITE_OVER 是合适的。
+            // 对于性能要求极高的场景，可以尝试使用 Imagick::COMPOSITE_COPY，但这种方式不会混合像素，而是直接复制源图像的像素到目标图像上，这可能会导致边缘出现锯齿或不自然的过渡。
+
+            $backgroundPath = $bgUrl;
+
+            // 创建一个Imagick对象来处理背景图片
+            $background = new \Imagick($backgroundPath);
+            $background->setImageFormat('png'); // 使用无损格式以保持质量（如果需要）
+
+            $tempFileArr = [];
+            if (!empty($data)) {
+                foreach ($data as $d) {
+                    $d = self::getRealData($d);
+                    if ($d['type'] == 'img' && !empty($d['src'])) {
+                        $retPath   = self::getImagePath($d['src']);
+                        $imagePath = $retPath['filePath'];
+                        $tempFile  = $retPath['tempFile'];
+                        if ($tempFile) {
+                            $tempFileArr[] = $tempFile;
+                        }
+                        if (is_file($imagePath)) {
+                            $mergeImage = new \Imagick($imagePath);
+                            $mergeImage->resizeImage($d['width'], $d['height'], \Imagick::FILTER_LANCZOS, 1);
+                            $background->compositeImage($mergeImage, \Imagick::COMPOSITE_OVER, $d['left'], $d['top']);
+                            $mergeImage->clear();
+                            $mergeImage->destroy();
+                        }
+                    } else if ($d['type'] == 'text') {
+                        $draw     = new \ImagickDraw();
+                        $rootPath = str_replace("\\", "/", dirname(dirname(dirname(dirname(__file__)))));
+                        $font     = $d['font'] ?? $rootPath . "/public/app/admin/static/fonts/msyh.ttf";
+                        $draw->setFont($font);
+                        $draw->setFontSize($d['size'] * 1.35);
+                        $draw->setFillColor(new \ImagickPixel($d['color']));
+
+                        $text = self::emoji($d['text']);
+                        $background->annotateImage($draw, $d['left'], $d['top'] + $d['size'], 0, $text); // 高度需要增加 40 不知道为什么
+                    }
+                }
+            }
+
+            $background->writeImage($imgPath);
+
+            // 清理资源
+            $background->clear();
+            $background->destroy();
+
+            // 删除临时文件
+            foreach ($tempFileArr as $tempFile) {
+                @unlink($tempFile);
+            }
+        }
+    }
+
+    // 获取文件路径(如果是远程地址就先下载在返回)
+    public static function getImagePath($url)
+    {
+        $tempFile = null; // 临时文件记得删除
+        if (!is_file($url) && strexists($url, 'http')) {
+            $correctedUrl = self::extractProtocolAndDomain($url);
+            $correctedUrl = str_replace($correctedUrl, "", $url);
+            $imagePath    = str_replace("/attachment/", "", $correctedUrl);
+
+            if (!is_file($url)) {
+                try {
+                    $tempFile = tempnam(sys_get_temp_dir(), 'img');
+                    ini_set('default_socket_timeout', 5);//设置默认超时时间为5秒
+                    $getImageData = file_get_contents($url);
+                    @file_put_contents($tempFile, $getImageData);
+                    $url = $tempFile;
+                } catch (Exception $exception) {
+                    $url = "";
+                }
+            } else {
+                $url = $imagePath;
             }
         }
 
-        imagejpeg($target, $imgPath); // 可接受的有损压缩
-        // imagepng($target, $imgPath); // 原图像无损清晰度
-        imagedestroy($target);
+        return [
+            'filePath' => $url,
+            'tempFile' => $tempFile,
+        ];
+    }
+
+
+    // 读取域名
+    public static function extractProtocolAndDomain($url)
+    {
+        // 正则表达式匹配http或https开头，后面跟着://，然后是任意非空白字符
+        preg_match('/^(https?:\/\/)?([\w.-]+)(\/.*)?$/i', $url, $matches);
+
+        // 提取出协议（可能为空）和域名
+        $protocol = isset($matches[1]) && $matches[1] ? $matches[1] : '';
+        $domain   = $matches[2];
+
+        return $protocol . $domain;
     }
 
     /**
@@ -52,11 +154,11 @@ class ImgUtil
      */
     public static function getRealData($data)
     {
-        $data['left'] = intval(str_replace('px', '', $data['left']));
-        $data['top'] = intval(str_replace('px', '', $data['top']));
-        $data['width'] = intval(str_replace('px', '', $data['width']));
+        $data['left']   = intval(str_replace('px', '', $data['left']));
+        $data['top']    = intval(str_replace('px', '', $data['top']));
+        $data['width']  = intval(str_replace('px', '', $data['width']));
         $data['height'] = intval(str_replace('px', '', $data['height']));
-        $data['size'] = intval(str_replace('px', '', $data['size']));
+        $data['size']   = intval(str_replace('px', '', $data['size']));
         return $data;
     }
 
@@ -87,7 +189,7 @@ class ImgUtil
     {
         if (!empty($rotated)) {
             $image_info = getimagesize($imgUrl);
-            $source = "";
+            $source     = "";
             switch ($image_info['mime']) {
                 case 'image/png':
                     // 处理 PNG 文件
@@ -127,10 +229,10 @@ class ImgUtil
     public static function mergeText($target, $text, $size, $color, $left, $top)
     {
         $rootPath = str_replace("\\", "/", dirname(dirname(dirname(dirname(__file__)))));
-        $font = $rootPath . "/public/app/admin/static/fonts/msyh.ttf";
+        $font     = $rootPath . "/public/app/admin/static/fonts/msyh.ttf";
 
         $colors = self::hex2rgb($color);
-        $color = imagecolorallocate($target, $colors['red'], $colors['green'], $colors['blue']);
+        $color  = imagecolorallocate($target, $colors['red'], $colors['green'], $colors['blue']);
 
         $text = self::emoji($text);
         $text = mb_convert_encoding(strval($text), "html-entities", "utf-8");
@@ -148,26 +250,26 @@ class ImgUtil
 
         // Match Emoticons
         $regexEmoticons = '/[\x{1F600}-\x{1F64F}]/u';
-        $clean_text = preg_replace($regexEmoticons, '', $text);
+        $clean_text     = preg_replace($regexEmoticons, '', $text);
 
         // Match Miscellaneous Symbols and Pictographs
         $regexSymbols = '/[\x{1F300}-\x{1F5FF}]/u';
-        $clean_text = preg_replace($regexSymbols, '', $clean_text);
+        $clean_text   = preg_replace($regexSymbols, '', $clean_text);
 
         // Match Transport And Map Symbols
         $regexTransport = '/[\x{1F680}-\x{1F6FF}]/u';
-        $clean_text = preg_replace($regexTransport, '', $clean_text);
+        $clean_text     = preg_replace($regexTransport, '', $clean_text);
 
         // Match Miscellaneous Symbols
-        $regexMisc = '/[\x{2600}-\x{26FF}]/u';
+        $regexMisc  = '/[\x{2600}-\x{26FF}]/u';
         $clean_text = preg_replace($regexMisc, '', $clean_text);
 
         // Match Dingbats
         $regexDingbats = '/[\x{2700}-\x{27BF}]/u';
-        $clean_text = preg_replace($regexDingbats, '', $clean_text);
+        $clean_text    = preg_replace($regexDingbats, '', $clean_text);
 
         $regexDingbats = '/[\x{231a}-\x{23ab}\x{23e9}-\x{23ec}\x{23f0}-\x{23f3}]/u';
-        $clean_text = preg_replace($regexDingbats, '', $clean_text);
+        $clean_text    = preg_replace($regexDingbats, '', $clean_text);
 
         return $clean_text;
     }
@@ -222,7 +324,7 @@ class ImgUtil
                     if (strpos($img, '?') !== false) {
 
                     } else {
-                        $im = array(
+                        $im       = array(
                             'old' => $img,
                             'new' => tomedia($img, $suffix)
                         );
@@ -246,9 +348,9 @@ class ImgUtil
                 foreach ($imgs[0] as $img) {
 
                     $pattern = "/src=(.*?)/";
-                    $newImg = preg_replace($pattern, "class=\"lazy-load\" src='/app/gm_arts/static/images/nopic.png' data-original=", $img);
+                    $newImg  = preg_replace($pattern, "class=\"lazy-load\" src='/app/gm_arts/static/images/nopic.png' data-original=", $img);
 
-                    $im = array(
+                    $im       = array(
                         'old' => $img,
                         'new' => $newImg
                     );
@@ -270,13 +372,13 @@ class ImgUtil
                 $videos[0] = array_unique($videos[0]);
                 foreach ($videos[0] as $videoStr) {
 
-                    $pattern = "/height=[\\\\'| \\\"](.*?)[\\\\'| \\\"]/";
+                    $pattern     = "/height=[\\\\'| \\\"](.*?)[\\\\'| \\\"]/";
                     $newVideoStr = preg_replace($pattern, "", $videoStr);
 
-                    $pattern1 = "/style=[\\\\'|\\\"](.*?)[\\\\'|\\\"]/";
+                    $pattern1    = "/style=[\\\\'|\\\"](.*?)[\\\\'|\\\"]/";
                     $newVideoStr = preg_replace($pattern1, " ", $newVideoStr);
 
-                    $im = array(
+                    $im          = array(
                         'old' => $videoStr,
                         'new' => $newVideoStr
                     );
