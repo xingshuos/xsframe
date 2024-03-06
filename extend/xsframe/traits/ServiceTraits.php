77bbc0993da4381a5f457765cc96047f28f20a04
zhaoxin <zx@baguatan.cn>
2024-03-06 10:47:33 +0800

    提供字符串查询的方式

--- extend/xsframe/traits/ServiceTraits.php
+++ extend/xsframe/traits/ServiceTraits.php
@@ -20,7 +20,8 @@ trait ServiceTraits
     public function getInfo($where, string $field = "*")
     {
         try {
-            $info = Db::name($this->tableName)->field($field)->where($where)->find();
+            extract(self::getWhere($where));
+            $info = Db::name($this->tableName)->field($field)->where($where, $op, $condition)->find();
         } catch (\Exception $exception) {
             $info = [];
         }
@@ -29,19 +30,20 @@ trait ServiceTraits
 
     /**
      * 分页获取数据
-     * @param array $condition
+     * @param array $where
      * @param string $field
      * @param string $order
      * @param int $pIndex
      * @param int $pSize
      * @return array
      */
-    public function getList(array $condition = array(), string $field = "*", string $order = "", int $pIndex = 1, int $pSize = 10): array
+    public function getList(array $where = array(), string $field = "*", string $order = "", int $pIndex = 1, int $pSize = 10): array
     {
         try {
+            extract(self::getWhere($where));
             if ($pIndex == 1 && !empty($this->params['page']) && $this->params['page'] > 1) $pIndex = $this->params['page'];
             if ($pSize == 10 && !empty($this->params['size']) && $this->params['size'] != 10) $pSize = $this->params['size'];
-            $list = Db::name($this->tableName)->field($field)->where($condition)->order($order)->page($pIndex, $pSize)->select()->toArray();
+            $list = Db::name($this->tableName)->field($field)->where($where, $op, $condition)->order($order)->page($pIndex, $pSize)->select()->toArray();
         } catch (\Exception $exception) {
             $list = [];
         }
@@ -50,19 +52,20 @@ trait ServiceTraits
 
     /**
      * 获取全部数据
-     * @param array $condition
+     * @param array $where
      * @param string $field
      * @param string $order
      * @param string $keyField
      * @return array
      */
-    public function getAll(array $condition = array(), string $field = "*", string $order = "", string $keyField = ''): array
+    public function getAll(array $where = array(), string $field = "*", string $order = "", string $keyField = ''): array
     {
         try {
+            extract(self::getWhere($where));
             if (empty($keyField)) {
-                $list = Db::name($this->tableName)->field($field)->where($condition)->order($order)->select()->toArray();
+                $list = Db::name($this->tableName)->field($field)->where($where, $op, $condition)->order($order)->select()->toArray();
             } else {
-                $temp = Db::name($this->tableName)->field($field)->where($condition)->order($order)->select()->toArray();
+                $temp = Db::name($this->tableName)->field($field)->where($where, $op, $condition)->order($order)->select()->toArray();
                 $rs = array();
                 if (!empty($temp)) {
                     foreach ($temp as $key => &$row) {
@@ -88,9 +91,10 @@ trait ServiceTraits
      * @return int
      * @throws DbException
      */
-    public function getTotal($condition, string $field = "*"): int
+    public function getTotal($where, string $field = "*"): int
     {
-        $total = Db::name($this->tableName)->where($condition)->count($field);
+        extract(self::getWhere($where));
+        $total = Db::name($this->tableName)->where($where, $op, $condition)->count($field);
         return intval($total);
     }
 
@@ -101,9 +105,10 @@ trait ServiceTraits
      * @param string $orderBy
      * @return int|mixed
      */
-    public function getValue($condition, $field, string $orderBy = 'id desc')
+    public function getValue($where, $field, string $orderBy = 'id desc')
     {
-        $total = Db::name($this->tableName)->where($condition)->order($orderBy)->value($field);
+        extract(self::getWhere($where));
+        $total = Db::name($this->tableName)->where($where, $op, $condition)->order($orderBy)->value($field);
         return $total ? $total : 0;
     }
 
@@ -114,9 +119,10 @@ trait ServiceTraits
      * @return bool
      * @throws DbException
      */
-    public function updateInfo(array $updateData, array $condition = []): bool
+    public function updateInfo(array $updateData, array $where = []): bool
     {
-        $isUpdate = Db::name($this->tableName)->where($condition)->update($updateData);
+        extract(self::getWhere($where));
+        $isUpdate = Db::name($this->tableName)->where($where, $op, $condition)->update($updateData);
         return (bool)$isUpdate;
     }
 
@@ -126,9 +132,10 @@ trait ServiceTraits
      * @return bool
      * @throws DbException
      */
-    public function deleteInfo($condition): bool
+    public function deleteInfo($where): bool
     {
-        $isDelete = Db::name($this->tableName)->where($condition)->delete();
+        extract(self::getWhere($where));
+        $isDelete = Db::name($this->tableName)->where($where, $op, $condition)->delete();
         return (bool)$isDelete;
     }
 
@@ -150,5 +157,17 @@ trait ServiceTraits
     public function insertAll($data): int
     {
         return Db::name($this->tableName)->insertAll($data);
+    }
+
+    // 获取查询条件
+    private function getWhere($where = null): array
+    {
+        $op = null;
+        $condition = null;
+        if (is_array($where) && is_string($where[0]) && is_array($where[1])) {
+            $op = $where[1];
+            $where = $where[0];
+        }
+        return ['where' => $where, 'op' => $op, 'condition' => $condition];
     }
 }
