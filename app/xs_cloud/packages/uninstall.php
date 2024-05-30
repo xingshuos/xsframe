bdd3ced27ef7edf4f90307b78f54a4352022e367
GuiHai <786824455@qq.com>
2024-05-30 14:48:23 +0800

    更新

--- app/xs_cloud/packages/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sql = "
-   DROP TABLE IF EXISTS `ims_cloud_app`;
-   DROP TABLE IF EXISTS `ims_cloud_app_version`;
-   DROP TABLE IF EXISTS `ims_cloud_frame_version`;
-";
-
-return $sql;
--- app/xs_cloud/packages/upgrade.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$sql = "
-   
-";
-
-return $sql;
