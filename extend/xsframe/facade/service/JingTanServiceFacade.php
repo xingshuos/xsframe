add382c4335dd52c2736a3c08d3b21b028e6cb5c
GuiHai <786824455@qq.com>
2024-06-06 17:45:49 +0800

    优化

--- extend/xsframe/facade/service/JingTanServiceFacade.php
+++ extend/xsframe/facade/service/JingTanServiceFacade.php
@@ -14,7 +14,7 @@ use xsframe\service\JingTanService;
  * @method static getUserInfo(mixed $accessToken)
  * @method static getAllAssetListByMobile($mobile, false|mixed $reload = false)
  * @method static grantAssetBySkuId(string $skuId, string $toIdNo, string $orderNo, float $priceCent = 0, string $toIdType = 'PHONE_NO')
- * @method static getNftInfoByTenantId(string $idNo, string $idType, int $pIndex = 1, int $pSize = 10)
+ * @method static getNftInfoByTenantId(string $idNo, string $idType = 'PHONE_NO', int $pIndex = 1, int $pSize = 10)
  * @method static applyNftBySkuId(string $skuId, string $toIdNo, string $orderNo, string $tenantId = null, string $idType = 'PHONE_NO')
  */
 class JingTanServiceFacade extends BaseFacade
