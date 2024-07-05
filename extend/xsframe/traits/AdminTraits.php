064b01287328c386f59155ffb9be402c47a678ea
GuiHai <786824455@qq.com>
2024-07-05 09:49:31 +0800

    优化自动添加问题

--- extend/xsframe/traits/AdminTraits.php
+++ extend/xsframe/traits/AdminTraits.php
@@ -182,7 +182,7 @@ trait AdminTraits
                 $updateData = [];
                 foreach ($fieldList as $filed => $fieldItem) {
 
-                    if ($id && !array_key_exists($filed, $this->params)) {
+                    if ($filed != 'uniacid' && !array_key_exists($filed, $this->params)) {
                         continue;
                     }
 
