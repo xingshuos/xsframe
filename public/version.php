2c3ac649cf43a5cba62409a048c58dbb6ad761f6
GuiHai <786824455@qq.com>
2024-07-18 18:47:43 +0800

    更新版本

--- public/version.php
+++ public/version.php
@@ -10,5 +10,5 @@
 // | Author: guiHai <786824455@qq.com>
 // +----------------------------------------------------------------------
 
-define('IMS_VERSION', '1.0.6');
-define('IMS_VERSION_TIME', '1717041762');
+define('IMS_VERSION', '1.0.8');
+define('IMS_VERSION_TIME', '1721299649');
