c85099e8c7c7fe314657812e870b9154fe755878
GuiHai <786824455@qq.com>
2024-10-11 16:58:21 +0800

    更新基类

--- vendor/xsframe/framework/src/xsframe/service/DbService.php
+++ vendor/xsframe/framework/src/xsframe/service/DbService.php
@@ -2,10 +2,27 @@
 
 namespace xsframe\service;
 
+use think\App;
+use think\Request;
 use xsframe\base\BaseService;
 use xsframe\traits\ServiceTraits;
+use xsframe\wrapper\AccountHostWrapper;
+use xsframe\wrapper\SettingsWrapper;
 
 class DbService
 {
+    protected $request;
+    protected $header;
+    protected $app;
+    protected $params;
+
     use ServiceTraits;
+
+    public function __construct(Request $request, App $app)
+    {
+        $this->request = $request;
+        $this->header = $request->header();
+        $this->app = $app;
+        $this->params = $this->request->param();
+    }
 }
