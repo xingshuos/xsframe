e9be8750cdef3878d9462958ef6a5315ac2ff9d9
zhaoxin <zhaoxin@luyin>
2024-11-16 23:56:04 +0800

    更新结构

--- app/admin/config/menu.php
+++ app/admin/config/menu.php
@@ -11,41 +11,6 @@
 // +----------------------------------------------------------------------
 
 return [
-    'account' => [
-        'title'    => '商户',
-        'subtitle' => '商户管理',
-        'icon'     => 'icon-archive',
-        'items'    => [
-            [
-                'title' => '商户管理',
-                'route' => 'list',
-            ],
-            [
-                'title' => '独立域名',
-                'route' => 'host',
-            ],
-        ]
-    ],
-    'app'     => [
-        'title'    => '应用',
-        'subtitle' => '应用管理',
-        'icon'     => 'icon-th-large',
-    ],
-    'users'   => [
-        'title'    => '管理员',
-        'subtitle' => '管理员管理',
-        'icon'     => 'icon-user',
-        'items'    => [
-            [
-                'title' => '账号设置',
-                'route' => 'profile',
-            ],
-            [
-                'title' => '用户管理',
-                'route' => 'list',
-            ],
-        ]
-    ],
     'sysset'  => [
         'title'    => '系统',
         'subtitle' => '系统设置',
@@ -82,5 +47,105 @@ return [
             ],
         ]
     ],
+    'app'     => [
+        'title'    => '应用',
+        'subtitle' => '应用管理',
+        'icon'     => 'icon-th-large',
+        'items'    => [
+            [
+                'title' => '未安装应用',
+                'route' => '',
+            ],
+            [
+                'title' => '已安装应用',
+                'route' => '',
+            ],
+            [
+                'title' => '已停用的应用',
+                'route' => '',
+            ],
+            [
+                'title' => '回收站',
+                'route' => '',
+            ],
+            [
+                'title' => '推荐的应用',
+                'route' => '',
+            ],
+        ]
+    ],
+    'account' => [
+        'title'    => '商户',
+        'subtitle' => '商户管理',
+        'icon'     => 'icon-archive',
+        'items'    => [
+            [
+                'title' => '商户管理',
+                'route' => 'list',
+            ],
+            [
+                'title' => '独立域名',
+                'route' => 'host',
+            ],
+        ]
+    ],
+    'users'   => [
+        'title'    => '管理员',
+        'subtitle' => '管理员管理',
+        'icon'     => 'icon-user',
+        'items'    => [
+            [
+                'title' => '账号设置',
+                'route' => 'profile',
+            ],
+            [
+                'title' => '用户管理',
+                'route' => 'list',
+            ],
+        ]
+    ],
+    'ops'     => [
+        'title'    => '运维',
+        'subtitle' => '运维管理',
+        'icon'     => 'icon-user',
+        'items'    => [
+            [
+                'title' => '系统运行情况',
+                'route' => '',
+            ],
+            [
+                'title' => '缓存处理',
+                'route' => '',
+            ],
+            [
+                'title' => '操作日志',
+                'route' => '',
+            ],
+            [
+                'title' => '检查BOM',
+                'route' => '',
+            ],
+            [
+                'title' => '数据库优化',
+                'route' => '',
+            ],
+        ]
+    ],
+    'college' => [
+        'title'    => '学院',
+        'subtitle' => '星数学院',
+        'icon'     => 'icon-user',
+        'items'    => [
+            [
+                'title' => '发布文章',
+                'route' => '',
+            ],
+            [
+                'title' => '系统图标',
+                'route' => '',
+            ],
+        ]
+    ],
+
 ];
 
