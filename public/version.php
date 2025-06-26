a1a76aaebf8cc4b030321406ebd1486e2eb7c5e6
GuiHai <786824455@qq.com>
2025-06-26 16:04:30 +0800

    更新版本信息

--- public/version.php
+++ public/version.php
@@ -10,5 +10,5 @@
 // | Author: guiHai <786824455@qq.com>
 // +----------------------------------------------------------------------
 
-const IMS_VERSION = '1.0.38';
-const IMS_VERSION_TIME = '1748596033';
+const IMS_VERSION = '1.0.39';
+const IMS_VERSION_TIME = '1750925053';
