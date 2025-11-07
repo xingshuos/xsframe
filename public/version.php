4fd3e08a5dcff55cf56d382fd49e22f353745555
GuiHai <786824455@qq.com>
2025-11-07 18:00:20 +0800

    更新版本

--- public/version.php
+++ public/version.php
@@ -10,5 +10,5 @@
 // | Author: guiHai <786824455@qq.com>
 // +----------------------------------------------------------------------
 
-const IMS_VERSION = '1.0.44';
-const IMS_VERSION_TIME = '1754644034';
+const IMS_VERSION = '1.0.45';
+const IMS_VERSION_TIME = '1762509605';
